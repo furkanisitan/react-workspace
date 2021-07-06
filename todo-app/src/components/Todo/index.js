85c5f199dfedf702671c7aa1f0544ad8209e4274
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-06 23:04:18 +0300

    Update Todo component

--- todo-app/src/components/Todo/index.js
+++ todo-app/src/components/Todo/index.js
@@ -1,16 +1,21 @@
 import Footer from 'components/Todo/Footer';
 import Form from 'components/Todo/Form';
+import List from 'components/Todo/List';
 import React from 'react';
 
 function Todo() {
     return (
         <section className="todoapp">
+
             <header className="header">
                 <h1>todos</h1>
                 <Form/>
             </header>
 
+            <List/>
+
             <Footer/>
+
         </section>
     );
 }
