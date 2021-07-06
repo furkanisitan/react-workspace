73ecdaa22c183fb13ab56fa90a32f0e380e9c990
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-06 18:11:26 +0300

    Create contacts components

---
+++ contacts-app/src/components/Contacts/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from 'components/Contacts/Form';
+import List from 'components/Contacts/List';
+
+function Contacts() {
+    return (
+        <div>
+            <List/>
+            <Form/>
+        </div>
+    );
+}
+
+export default Contacts;
