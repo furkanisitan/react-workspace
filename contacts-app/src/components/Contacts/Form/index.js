0f392b3ca856f278eefdd144037d8519682982d2
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-06 18:31:28 +0300

    Implement Contacts Form

--- contacts-app/src/components/Contacts/Form/index.js
+++ contacts-app/src/components/Contacts/Form/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-function Form() {
+const initialFormValues = {fullName: '', phoneNumber: ''};
+
+function Form({setContacts, contacts}) {
+
+    const [form, setForm] = useState(initialFormValues);
+
+    useEffect(() => {
+        setForm(initialFormValues);
+    }, [contacts]);
+
+    const onChangeInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.fullName === '' || form.phoneNumber === '') {
+            return false;
+        }
+
+        setContacts([...contacts, form]);
+    };
+
     return (
-        <div>
-            Contact Form
-        </div>
+        <form onSubmit={onSubmit}>
+            <div>
+                <input
+                    name="fullName"
+                    placeholder="Full Name"
+                    value={form.fullName}
+                    onChange={onChangeInput}
+                />
+            </div>
+
+            <div>
+                <input
+                    name="phoneNumber"
+                    placeholder="Phone Number"
+                    value={form.phoneNumber}
+                    onChange={onChangeInput}
+                />
+            </div>
+
+            <div className="btn">
+                <button type="submit">Add</button>
+            </div>
+        </form>
     );
 }
 
