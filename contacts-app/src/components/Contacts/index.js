b51fe00a29e5d58256962559ba3c6708ffb3b403
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-06 18:51:54 +0300

    Implement Contacts component

--- contacts-app/src/components/Contacts/index.js
+++ contacts-app/src/components/Contacts/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
 import Form from 'components/Contacts/Form';
 import List from 'components/Contacts/List';
+import React, {useState} from 'react';
+
+const initialContacts = [
+    {fullName: 'User 1', phoneNumber: '123456789'},
+    {fullName: 'User 2', phoneNumber: '123456788'},
+    {fullName: 'User 3', phoneNumber: '123456787'}
+];
 
 function Contacts() {
+
+    const [contacts, setContacts] = useState(initialContacts);
+
     return (
         <div>
-            <List/>
-            <Form/>
+            <h1>Contacts</h1>
+            <List contacts={contacts}/>
+            <Form contacts={contacts} setContacts={setContacts}/>
         </div>
     );
 }
