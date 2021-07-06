3901f9dcc9d488e0fdf21e981c4465bcd6353c3b
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-06 22:26:50 +0300

    Create Todo and Footer components

---
+++ todo-app/src/components/Todo/index.js
@@ -0,0 +1,18 @@
+import Footer from 'components/Todo/Footer';
+import Form from 'components/Todo/Form';
+import React from 'react';
+
+function Todo() {
+    return (
+        <section className="todoapp">
+            <header className="header">
+                <h1>todos</h1>
+                <Form/>
+            </header>
+
+            <Footer/>
+        </section>
+    );
+}
+
+export default Todo;
