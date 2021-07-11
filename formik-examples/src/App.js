31507d33e92c9c6b59f686574d1cdbf6f56b1ddb
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-11 09:22:35 +0300

    Create SemanticRegisterForm component

--- formik-examples/src/App.js
+++ formik-examples/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import {Link, Route, Switch} from 'react-router-dom';
+import SemanticRegisterForm from './components/User/SemanticRegisterForm';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">SemanticRegisterForm</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Switch>
+                <Route path={['/', '/semantic-register-form']} component={SemanticRegisterForm}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ formik-examples/src/components/User/SemanticRegisterForm/index.js
@@ -0,0 +1,51 @@
+import {Field, Form, Formik} from 'formik';
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+const formInitialValues = {
+    fullName: '',
+    email: '',
+    password: '',
+    gender: 'male',
+    city: '',
+    dateOfBirth: '',
+    isAgreed: false
+};
+
+function SemanticRegisterForm() {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: ''
+                }}
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 500));
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                <Form>
+                    <label htmlFor="firstName">First Name</label>
+                    <Field id="firstName" name="firstName" placeholder="Jane"/>
+
+                    <label htmlFor="lastName">Last Name</label>
+                    <Field id="lastName" name="lastName" placeholder="Doe"/>
+
+                    <label htmlFor="email">Email</label>
+                    <Field
+                        id="email"
+                        name="email"
+                        placeholder="jane@acme.com"
+                        type="email"
+                    />
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+        </div>
+    );
+}
+
+export default SemanticRegisterForm;
