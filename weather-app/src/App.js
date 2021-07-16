502b10af02b6121b7eb694a33f130bc088747914
Furkan Işıtan <furkanisitan96@gmail.com>
2021-07-16 15:35:16 +0300

    Create App.css

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,28 @@
 import Weather from 'components/Weather';
+import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import {Container} from 'semantic-ui-react';
+import {Card, Container, Dropdown, Segment} from 'semantic-ui-react';
+import './App.css';
+
+const cityOptions = [
+    {key: 1, value: 'Adana', text: 'Adana'},
+    {key: 2, value: 'Adıyaman', text: 'Adıyaman'},
+    {key: 3, value: 'Afyonkarahisar', text: 'Afyonkarahisar'}
+];
 
 function App() {
     return (
-        <div>
-            <Container>
-                <Weather/>
-            </Container>
-        </div>
+        <Container className="app">
+
+            <h2>Wheather App</h2>
+
+            <div className="city-dropdown">
+                <Dropdown placeholder="Şehir Seçin" search selection options={cityOptions}/>
+            </div>
+
+            <Weather/>
+
+        </Container>
     );
 }
 
